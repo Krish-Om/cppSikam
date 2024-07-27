b3103570833a1bdea31c08d343d599516c4b36b9
Krish-Om <Krishombasukala@gmail.com>
2024-07-27 20:07:52 +0545

    refactor: finished refactoring the RPS project

--- RPSGame/Main.c++
+++ RPSGame/Main.c++
@@ -1,4 +1,4 @@
-// #include "Game.hpp"
+// #include "RPSGame.hpp"
 #include "Game.hpp"
 #include "iostream"
 using namespace std;
--- RPSGame/cmake-build-debug/cmake_install.cmake
+++ RPSGame/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/krishom/Workspace/collab/Game
+# Install script for directory: /home/krishom/Workspace/collab/RPSGame
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/krishom/Workspace/collab/Game/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/krishom/Workspace/collab/RPSGame/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
