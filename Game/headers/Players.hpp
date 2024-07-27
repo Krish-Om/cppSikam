b8ae5cb1a606518b06d9d4ec4d9eaeb5abb32fab
Krish-Om <Krishombasukala@gmail.com>
2024-07-27 16:17:39 +0545

    fix: fixed the compilation error using cmake feat: Finish working on feature multiplayer

--- Game/headers/Players.hpp
+++ Game/headers/Players.hpp
@@ -4,23 +4,23 @@
 
 #ifndef GAME_PLAYERS_HPP
 #define GAME_PLAYERS_HPP
-#include "User_Player.hpp"
+
 enum userChoice {
     ROCK = 1, PAPER = 2, SCISSOR = 3
 };
-class Players{
-private:
 
+class Players {
+private:
     unsigned int choice;
 public:
     Players();
     void setState(unsigned int ch);
     int getState();
-    virtual void setChoice()=0;
+    virtual void setChoice() = 0;
     static void choiceInfo();
     int compare(Players &p2);
-    static void determineWinner(Players &p1, Players& b);
-
-    ~Players();
+    static void determineWinner(Players &p1, Players &b);
+    virtual ~Players();
 };
+
 #endif //GAME_PLAYERS_HPP
--- Game/source/Bot.c++
+++ Game/source/Bot.c++
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-#include "Bot.hpp"
+#include "../headers/Bot.hpp"
 
 
 void Bot::setChoice()
